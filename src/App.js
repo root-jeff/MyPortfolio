35d7393fd0c3a0190e1a57ab57e834c658e47a01
Jeff <jpalmacoloma@gmail.com>
2023-12-02 02:01:57 -0500

    Otras variables

--- src/App.js
+++ src/App.js
@@ -7,11 +7,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Balaa balaaaaa sapo mmv
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          className="Un regalito miamor"
+          href="https://youtu.be/jhWkOCkXdKU"
           target="_blank"
           rel="noopener noreferrer"
         >
