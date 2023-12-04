93b8f328dea8de4d3996a926e86fa72a099f5110
Jeff <jpalmacoloma@gmail.com>
2023-12-03 23:21:50 -0500

    Production

--- src/Home/Data.tsx
+++ src/Home/Data.tsx
@@ -1,13 +1,13 @@
 import React from "react";
-import {hand} from "../assets/hand.tsx"
-import {send} from "../assets/send.tsx"
+import { hand } from "../assets/hand.tsx";
+import { send } from "../assets/send.tsx";
 
 export const Data = () => {
   return (
     <div className="home__data">
       <h1 className="home__title">
-        Jefferson 
-       {hand()}
+        Jefferson
+        {hand()}
       </h1>
       <h3 className="home__subtitle">Desarollo web zzzz</h3>
       <p className="home__description">Otros zzzzzz quemados por el momento</p>
--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -1,19 +1,16 @@
 import React from "react";
-import "./home.css"
+import "./home.css";
 import { Data } from "./Data.tsx";
 import { Social } from "./Social.tsx";
 
 export const Home = () => {
-
   return (
     <section className="home section" id="home">
       <div className="home__container container grid">
         <div className="home__content grid">
-          <Social/>
-          <div className="home__img">
-
-          </div>
-          <Data/>
+          <Social />
+          <div className="home__img"></div>
+          <Data />
         </div>
       </div>
     </section>
